403c64d79f471f12c19a4b64965d5e6f7149b88b
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-18 10:50:17 -0300

    adiciona conceitos de Variáveis e tipos de Dados

---
+++ logica-python/variaveis.py
@@ -0,0 +1,21 @@
+nome = "Daniel"
+idade = 18
+altura = 1.65
+dev = True
+
+print(f"Olá, {nome}! Você tem {idade} anos e mede {altura}m.")
+
+nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobre nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"Olá, {nome} {sobrenome}! Você tem {idade} anos e mede {altura}m.")
+
+esporte = input("Qual seu esporte favorito? ")
+comida = input("Qual sua comida favorita? ")
+fimel = input("Qual seu filme favorito? ")
+cor = input("Qual sua cor favorita? ")
+
+print(f"Seu Esporte é {esporte}, sua comida favorita é {comida}, seu filme {fimel} e sua cor favorita é {cor}")
+
