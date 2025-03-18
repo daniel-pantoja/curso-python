268a1372a949d320a74ef3b941d09dbee9cb8d0f
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-18 20:18:23 -0300

    adicionar Operadores

---
+++ logica-python/operadores.py
@@ -0,0 +1,28 @@
+soma = 3 + 4           # Adição
+subtracao = 4 - 3      # Subtração
+multiplicacao = 10 * 10  # Multiplicação
+divisao = 10 / 2        # Divisão
+modulo = 5 % 2         # Resto da Divisão
+potencia = 4 ** 2     # Potência
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
+print("Módulo:", modulo)
+print("Potência:", potencia)
+
+x = 5
+y = 10
+print("x é igual a y?", x == y)        # Igualdade
+print("x é diferente de y?", x != y)   # Diferença
+print("x é maior que y?", x > y)       # Maior
+print("x é menor que y?", x < y)       # Menor
+print("x é maior ou igual a y?", x >= y)  # Maior ou igual
+print("x é menor ou igual a y?", x <= y)  # Menor ou igual
+
+# Operadores Lógicos
+a = True
+b = False
+print("a e b:", a and b)     # E lógico
+print("a ou b:", a or b)     # Ou lógico
+print("não a:", not a)       # Negação
