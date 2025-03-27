69a9d24f0d364877a27b40a655387bf3c77656e1
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-27 12:17:37 -0300

    adicionar exercício 003 em Python

---
+++ estudonauta-python/exercicios/ex003.py
@@ -0,0 +1,5 @@
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('Digite outro número: '))
+res = numero1 + numero2
+
+print('A soma entre os valores {} e {} tem o resultado de {}'.format(numero1, numero2, res))
