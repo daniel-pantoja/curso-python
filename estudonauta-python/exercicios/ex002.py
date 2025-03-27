923fb650b903ca74c68196d8dacf62692bb63ab9
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-27 10:42:25 -0300

    adicionar exercício 002 em Python

---
+++ estudonauta-python/exercicios/ex002.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua Idade: ')
+
+print('É um grande prazer te conhecer', nome)
+print('É um grande prazer te conhecer {}'.format(nome))
+
+print('Você tem ' + idade + ' anos')
+print('Você tem {} anos'.format(idade))
