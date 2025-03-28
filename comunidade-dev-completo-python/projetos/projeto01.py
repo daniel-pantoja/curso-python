bf82473600463073752ba932610147e99eab70c0
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-28 14:36:23 -0300

    projeto de número secreto

--- comunidade-dev-completo-python/projetos/projeto01.py
+++ comunidade-dev-completo-python/projetos/projeto01.py
@@ -3,3 +3,15 @@
 # No jogo, o usuário precisa adivinhar um número secreto
 # Ele pode tentar várias vezes até acertar. 
 
+numero_secreto = 10
+tentativa = 0
+
+while tentativa != numero_secreto:
+  tentativa = int(input("Adivinhe um núme de 1 a 10: "))
+
+  if tentativa < numero_secreto:
+    print("O número secreto é maior!")
+  elif tentativa > numero_secreto: 
+    print("O número secreto é menor!") 
+  else: 
+    print("Você acertou o número, parabêns!!!")
