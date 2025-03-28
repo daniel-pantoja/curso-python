cefbd3450d2d03a54088a6f0baf4f32ed26db3e9
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-28 15:38:26 -0300

    adiciona Variáveis e While no projeto

--- comunidade-dev-completo-python/projetos/projeto02.py
+++ comunidade-dev-completo-python/projetos/projeto02.py
@@ -1,3 +1,11 @@
 # Simulando um Caixa Eletrônico
 
 # O usuário tem um saldo inicial de R$ 500 e pode sacar dinheiro até o saldo zerar ou encerrar.
+
+saldo = 500
+saque = 0
+
+
+while saldo > 0:
+  saque = float(input("Informe o valor do saque: "))
+  
