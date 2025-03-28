db092388ec964a4760ae69c3bfa5c647e2832056
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-28 13:53:51 -0300

    adiciona União de Conceitos em Python

---
+++ comunidade-dev-completo-python/repcond.py
@@ -0,0 +1,13 @@
+# União de Conceitos
+
+palavra = "Daniel"
+
+for letra in palavra:
+  if letra == "D":
+    print("Essa palavra tem a letra D!")
+
+nome = "Junior"
+letraProcurada = "i"
+for letra in palavra:
+  if letra == letraProcurada:
+    print("Essa palavra tem a letra {}!".format(letraProcurada))
