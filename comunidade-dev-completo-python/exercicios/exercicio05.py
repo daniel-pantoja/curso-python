f45fc1a90a2382cfb0ac50274ff5d19080ef5033
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-30 11:43:04 -0300

    adiciona exercício-5 na linguagem de programação Python

---
+++ comunidade-dev-completo-python/exercicios/exercicio05.py
@@ -0,0 +1,8 @@
+# É bem direto. Seu objetivo é criar uma função que remova o primeiro e o último caractere de uma string.
+# Você não precisa se preocupar com strings com menos de dois caracteres. 
+
+# Lógica:
+# Não precisamos nos preocupar com strings de comprimento menor que dois caracteres, o que simplifica nossa lógica.
+
+def remove_chat(s):
+    return s[1:-1]
