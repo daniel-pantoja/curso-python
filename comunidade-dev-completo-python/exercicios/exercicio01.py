bd6b1dcd171171a839fead776a72572becad0e28
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-29 21:03:04 -0300

    adiciona exercício-1

---
+++ comunidade-dev-completo-python/exercicios/exercicio01.py
@@ -0,0 +1,11 @@
+# Comando:
+# Este código não executa corretamente. Tente descobrir o porquê.
+
+# Lógica:
+# Fazer um retorno no código para gerar um resultado
+
+def multiplay(a, b):
+    return a * b
+
+resultado = multiplay(2, 2)
+print(f"Resultado da multiplicação é {resultado}")
