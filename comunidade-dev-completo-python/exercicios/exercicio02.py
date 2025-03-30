3bec642d479de8c79d96fbffad3f83f0498fe3b5
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-30 10:27:51 -0300

    adiciona exercício-2

---
+++ comunidade-dev-completo-python/exercicios/exercicio02.py
@@ -0,0 +1,4 @@
+# Escreva uma função que aceite um inteiro não negativo n e uma string s como parametros e retorne uma string de vezes s exatas repetidas n .
+
+def repeat_str(repeat, string):
+    return repeat * string
