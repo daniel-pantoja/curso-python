1f4cec1eb6d7ce3053156bb9d7a3492f1c7d9b5a
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-30 10:58:35 -0300

    adiciona exercício-4

---
+++ comunidade-dev-completo-python/exercicios/exercicio04.py
@@ -0,0 +1,8 @@
+# Muito simples, dado um número (inteiro/decimal/ambos, dependendo do idioma), encontre seu oposto (inverso aditivo).
+
+# Lógica:
+# Pegar o número e multíplicar por -1.
+# Assim vai ter a transformação de número para seu oposto.
+
+def oppsite(number):
+    return number * -1
