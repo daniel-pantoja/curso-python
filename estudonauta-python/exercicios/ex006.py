9d8ea5b2310e60159e337630d00108ba44589ffc
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-31 09:57:40 -0300

    acrescenta exercício-5 Dobro, Triplo, Raiz Quadrada

---
+++ estudonauta-python/exercicios/ex006.py
@@ -0,0 +1,10 @@
+# Cria um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int(input('Digite um número: ')) 
+dobro = n * 2
+triplo = n ** 3
+raiz = n ** (1/2)
+
+print(f"O dobro de {n} vale {dobro}")
+print(f"O triplo de {n} vale {triplo}")
+print(f"A raiz de {n} é igual a {raiz}")
