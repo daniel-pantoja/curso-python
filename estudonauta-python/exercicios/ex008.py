d9b7e256aea449a82c1feb836315f3a5fe3ece6d
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-31 10:46:34 -0300

    acrescenta exercício-5 Conversor de Medidas

---
+++ estudonauta-python/exercicios/ex008.py
@@ -0,0 +1,11 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros.
+
+medida = float(input('Uma distânia em metros: '))
+km = medida / 1000
+hm = medida / 100
+dam = medida / 10
+dm = medida * 10
+cm = medida * 100
+mm = medida * 1000
+
+print(f'A medida de {medida}m corresponde a \n{km}km \n{hm}hm \n{dam}dam \n{dm:.0f}dm  \n{cm:.0f}cm \n{mm:.0f}mm')
