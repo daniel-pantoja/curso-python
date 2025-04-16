84dd5344cad72b63f66eb900273776ad05c73a4a
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-04-16 15:38:04 -0300

    adiciona conceitos de Cores no Terminal

--- estudonauta-python/aulas/aula11.py
+++ estudonauta-python/aulas/aula11.py
@@ -1,1 +1,8 @@
 # Cores no Terminal
+# print('\033[31mOlá, Mundo!\033[0m')
+
+print('\033[7;33;44mOlá, Mundo!\033[m')
+
+a = 3 
+b = 4
+print(f'Os valores são \033[32m{a}\033[m e \033[31m{b}\033[m!!!')
